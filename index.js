986fe977bf2a9cf94c09b1ebf2e538e14b31d7cc
yolendraf <yolendrianisyahfitri@gmail.com>
2023-11-16 21:02:09 +0700

    swap tittle

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Mengambil form elemen
-const formElement = document.querySelector('form-todo');
+const formElement = document.querySelector('.form-todo');
 
 // Menambahkan form event listener
 formElement.addEventListener('submit', (event) => {
     event.preventDefault();
 
     // Mengambil input elemen di html
-    const inputElemen = document.querySelector('.input-todo');
+    const inputElement = document.querySelector('.input-todo');
     console.log(inputElement.value);
 
     // Mengambil wrapper todo element sebagai entry point todo item
@@ -15,12 +15,12 @@ formElement.addEventListener('submit', (event) => {
     // Membuat todo item di js
     // const todoItem = `<li class="todo-item">${inputElement.value}</li>`;
     const todoItem = document.createElement('li');
-    todoItem.innerHTML = inputElemen.value;
+    todoItem.innerHTML = inputElement.value;
 
     // Menambahkan todo item kedalam wrapper todo
     // wrapperTodoElement.innerHTML = todoItem;
     wrapperTodoElement.append(todoItem);
 
     // Menghapus input value setelah submit
-    inputElemen.value = '';
+    inputElement.value = '';
 });
